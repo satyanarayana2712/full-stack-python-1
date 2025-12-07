a31804ced351192c217dd6662c5f13f9f5e858d5
satyanarayana2712 <satyanarayanaala88@gmail.com>
2025-12-07 18:25:47 +0530

    Prepare Django for deployment on Render

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_yqxuy6s$(*tqz_8jbq_78j*3@nkwqcw2(6bxx1je9makcnj1(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 CORS_ALLOWED_ORIGINS=[
     "http://localhost:3000",
     "http://127.0.0.1:3000",
